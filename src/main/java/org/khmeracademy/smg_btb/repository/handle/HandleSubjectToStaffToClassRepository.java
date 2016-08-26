7f1bd12d92b85cc8aff4e00dd66e5ca6fa33d859
hengmengtang <heng.mengtang@gmail.com>
2016-08-26 15:00:44 +0700

    add

--- src/main/java/org/khmeracademy/smg_btb/repository/handle/HandleSubjectToStaffToClassRepository.java
+++ src/main/java/org/khmeracademy/smg_btb/repository/handle/HandleSubjectToStaffToClassRepository.java
@@ -11,11 +11,11 @@ public interface HandleSubjectToStaffToClassRepository {
 
 	interface SQL{
 		final String HANDLE_SUBJECT_TO_STAFF_TO_CLASS="{CALL handle_subject_to_staff_to_class("
+				+ "#{generation_name,jdbcType=VARCHAR,mode=IN},"
 				+ "#{staff_handle_name,jdbcType=VARCHAR,mode=IN},"
 				+ "#{class_room_name,jdbcType=VARCHAR,mode=IN},"
 				+ "#{course_name,jdbcType=VARCHAR,mode=IN},"
-				+ "#{subject_type_name,jdbcType=VARCHAR,mode=IN},"
-				+ "#{generation_name,jdbcType=VARCHAR,mode=IN},"
+				+ "#{subject_type_name,jdbcType=VARCHAR,mode=IN},"			
 				+ "#{date,jdbcType=VARCHAR,mode=IN},"
 				+ "#{success,jdbcType=INTEGER,mode=OUT})}";
 	}
